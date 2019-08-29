78cbf26bd976991b15e85aca148a08a97f9bc858
Sho Hirose <sho.hirose@utexas.edu>
2019-08-29 10:53:04 +0900

    test: refactor tests using Google Test

--- test.cpp
+++ test.cpp
@@ -1,42 +1,51 @@
-#include <iostream>
+#include <gtest/gtest.h>
 #include <vector>
 #include "sort.hpp"
 
-void print(const std::vector<int>& v) {
-  for (auto&& vi : v) std::cout << vi << ' ';
-  std::cout << std::endl;
+namespace sh = shirose;
+
+class SortTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    v_ = std::vector<int>({10, 3, 1, 4, 2, 6, 4, 5, 7, 8, 9, 7});
+    v_ans_ = std::vector<int>({1, 2, 3, 4, 4, 5, 6, 7, 7, 8, 9, 10});
+  }
+
+  std::vector<int> v_;
+  std::vector<int> v_ans_;
+};
+
+TEST_F(SortTest, BubbleSort) {
+  sh::bubble_sort(v_.begin(), v_.end());
+  for (size_t i = 0; i < v_.size(); ++i) EXPECT_EQ(v_[i], v_ans_[i]);
 }
 
-int main() {
-  using namespace shirose;
+TEST_F(SortTest, SelectionSort) {
+  sh::selection_sort(v_.begin(), v_.end());
+  for (size_t i = 0; i < v_.size(); ++i) EXPECT_EQ(v_[i], v_ans_[i]);
+}
 
-  std::vector<int> v = {10, 3, 1, 2, 6, 4, 5, 8, 9, 7};
-  print(v);
+TEST_F(SortTest, ShakerSort) {
+  sh::shaker_sort(v_.begin(), v_.end());
+  for (size_t i = 0; i < v_.size(); ++i) EXPECT_EQ(v_[i], v_ans_[i]);
+}
 
-  bubble_sort(v.begin(), v.end());
-  print(v);
+TEST_F(SortTest, CombSort) {
+  sh::comb_sort(v_.begin(), v_.end());
+  for (size_t i = 0; i < v_.size(); ++i) EXPECT_EQ(v_[i], v_ans_[i]);
+}
 
-  v = {10, 3, 1, 2, 6, 4, 5, 8, 9, 7};
-  selection_sort(v.begin(), v.end());
-  print(v);
+TEST_F(SortTest, NormSort) {
+  sh::norm_sort(v_.begin(), v_.end());
+  for (size_t i = 0; i < v_.size(); ++i) EXPECT_EQ(v_[i], v_ans_[i]);
+}
 
-  v = {10, 3, 1, 2, 6, 4, 5, 8, 9, 7};
-  shaker_sort(v.begin(), v.end());
-  print(v);
+TEST_F(SortTest, ShellSort) {
+  sh::shell_sort(v_.begin(), v_.end());
+  for (size_t i = 0; i < v_.size(); ++i) EXPECT_EQ(v_[i], v_ans_[i]);
+}
 
-  v = {10, 3, 1, 2, 6, 4, 5, 8, 9, 7};
-  comb_sort(v.begin(), v.end());
-  print(v);
-
-  v = {10, 3, 1, 2, 6, 4, 5, 8, 9, 7};
-  norm_sort(v.begin(), v.end());
-  print(v);
-
-  v = {10, 3, 1, 2, 6, 4, 5, 8, 9, 7};
-  shell_sort(v.begin(), v.end());
-  print(v);
-
-  v = {10, 3, 1, 2, 6, 4, 5, 8, 9, 7};
-  insertion_sort(v.begin(), v.end());
-  print(v);
+TEST_F(SortTest, InsertionSort) {
+  sh::insertion_sort(v_.begin(), v_.end());
+  for (size_t i = 0; i < v_.size(); ++i) EXPECT_EQ(v_[i], v_ans_[i]);
 }
